75cfd11a16f9b5ef4c61480bb66944c4589a2417
Marcelo Santos <rocha.mer21@gmail.com>
2022-09-08 13:33:17 -0300

    Saving a User to Firestore Database - Test User

--- app/src/main/java/com/marcelo/readerapp/MainActivity.kt
+++ app/src/main/java/com/marcelo/readerapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.marcelo.readerapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,6 +11,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.firestore.FirebaseFirestore
 import com.marcelo.readerapp.ui.theme.ReaderAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,11 +19,25 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ReaderAppTheme {
+
+                val db = FirebaseFirestore.getInstance()
+                val user: MutableMap<String, Any> = HashMap()
+                user["firstName"] = "Marcelo"
+                user["lastName"] = "Santos"
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    db.collection("users")
+                        .add(user)
+                        .addOnSuccessListener {
+                            Log.d("FIRE", "onCreate: ${it.id}")
+                        }.addOnFailureListener {
+                            Log.d("FIRE", "onCreate: ${it.message}")
+                        }
+
                     Greeting("Android")
                 }
             }
