ed232891e02a87c3e17c4e856baa58f8fefc9a11
Marcelo Santos <rocha.mer21@gmail.com>
2022-09-09 11:16:48 -0300

    create: enum class ReaderScreens and companion object

---
+++ app/src/main/java/com/marcelo/readerapp/navigation/ReaderScreens.kt
@@ -0,0 +1,26 @@
+package com.marcelo.readerapp.navigation
+
+enum class ReaderScreens {
+    SplashScreen,
+    LoginScreen,
+    CreateAccountScreen,
+    ReaderHomeScreen,
+    SearchScreen,
+    DetailScreen,
+    UpdateScreen,
+    ReaderStatsScreen;
+
+    companion object {
+        fun fromRoute(route: String?): ReaderScreens = when (route?.substringBefore("/")) {
+            SplashScreen.name -> SplashScreen
+            LoginScreen.name -> LoginScreen
+            CreateAccountScreen.name -> CreateAccountScreen
+            SearchScreen.name -> SearchScreen
+            DetailScreen.name -> DetailScreen
+            UpdateScreen.name -> UpdateScreen
+            ReaderStatsScreen.name -> ReaderStatsScreen
+            null -> ReaderHomeScreen
+            else -> throw IllegalArgumentException("Route $route is not recognized")
+        }
+    }
+}
