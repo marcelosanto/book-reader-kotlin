4ddd99ccd84060d7580785b9fd695adf66cf6921
Marcelo Santos <rocha.mer21@gmail.com>
2022-09-10 15:23:57 -0300

    create: splashscreen without animation

--- app/src/main/java/com/marcelo/readerapp/screens/ReaderSplashScreen.kt
+++ app/src/main/java/com/marcelo/readerapp/screens/ReaderSplashScreen.kt
@@ -1,18 +1,44 @@
 package com.marcelo.readerapp.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
 fun ReaderSplashScreen(navController: NavController) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    Surface(
+        modifier = Modifier
+            .padding(15.dp)
+            .size(330.dp),
+        shape = CircleShape,
+        color = Color.White,
+        border = BorderStroke(width = 2.dp, color = Color.LightGray)
     ) {
-        Text("SplashScreen")
+        Column(
+            modifier = Modifier.padding(1.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "A. Reader",
+                style = MaterialTheme.typography.h3,
+                color = Color.Red.copy(alpha = 0.5f)
+            )
+            Spacer(modifier = Modifier.height(15.dp))
+            Text(
+                text = "\"Read, Change, Yourself\"",
+                style = MaterialTheme.typography.h5,
+                color = Color.LightGray
+            )
+        }
     }
 }
